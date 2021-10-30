4e160fede5856b0a3dbea5bafc1ea1ce8ff7f0f0
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2021-10-30 15:28:25 -0400

    Add files via upload

--- gamepage.js
+++ gamepage.js
@@ -2,24 +2,68 @@ player1name = localStorage.getItem("player1name");
 player2name = localStorage.getItem("player2name");
 player1score = 0;
 player2score = 0;
+questionturn = "player1";
+answerturn = "player2";
 
 document.getElementById("player1name").innerHTML = player1name + ": ";
 document.getElementById("player2name").innerHTML = player2name + ": ";
 document.getElementById("player1score").innerHTML = player1score;
 document.getElementById("player2score").innerHTML = player2score;
-document.getElementById("playerquestion").innerHTML = "Question Turn - " + player1name;
-document.getElementById("playeranswer").innerHTML = "Answer Turn - " + player2name;
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1name;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2name;
 
 function send() {
-    number1 = document.getElementById("number1input").value;
-    number2 = document.getElementById("number2input").value;
-    actualanswer = parseInt(number1) * parseInt(number2);
+    getword = document.getElementById("word").value;
+    word = getword.toLowerCase();
 
-    question = "<h4>" + number1 + " * " + number2 + "</h4>";
-    inputbox = "<br> Answer: <input type='number' id='inputcheckbox'>";
-    checkbutton = "<br><br> <button class='btn btn-info' onclick='check()'> Check</button>";
-    row = question + inputbox + checkbutton;
+    char1 = word.charAt(1);
+    x = Math.floor(word.length/2);
+    char2 = word.charAt(x);
+    y = word.length - 1;
+    char3 = word.charAt(y);
+
+    replace1 = word.replace(char1, "_");
+    replace2 = replace1.replace(char2, "_");
+    replace3 = replace2.replace(char3, "_");
+    console.log(replace3);
+    
+    question = "<h4 id='displayword'> Q." + replace3 + "</h4>";
+    input = "<br> Answer: <input id='input_check_box'>";
+    buttoncheck = "<br> <br> <button class='btn btn-info' onclick='check()'>Check</button>"
+    row = question + input + buttoncheck;
     document.getElementById("output").innerHTML = row;
-    document.getElementById("number1input").value = "";
-    document.getElementById("number2input").value = "";
+    document.getElementById("word").value = "";
+}
+
+function check() {
+    getanswer = document.getElementById("input_check_box").value;
+    answer = getanswer.toLowerCase();
+    if (answer == word) {
+        if (answerturn == "player2"){
+            player2score += 1;
+            document.getElementById("player2score").innerHTML = player2score;
+        }
+        else {
+            player1score += 1;
+            document.getElementById("player1score").innerHTML = player1score;
+        }
+    }
+    if (questionturn == "player1") {
+        questionturn = "player2";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2name;
+    }
+    else {
+        questionturn = "player1";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1name;
+    }
+    if(answerturn == "player1") {
+        answerturn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2name;
+    }
+    else {
+        answerturn = "player1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1name;
+    }
+
+    document.getElementById("output").innerHTML = "";
 }
